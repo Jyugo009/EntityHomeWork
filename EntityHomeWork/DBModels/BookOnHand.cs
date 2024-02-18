3d4185afeb60e6234744f92c149818960b2c8881
nekrass06@gmail.com <nekrass06@gmail.com>
2024-02-18 23:02:53 +0200

    done second part of homework

---
+++ EntityHomeWork/DBModels/BookOnHand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityHomeWork.DBModels;
+
+public partial class BookOnHand
+{
+    public int Id { get; set; }
+
+    public int? BookId { get; set; }
+
+    public string? TakenBy { get; set; }
+
+    public DateTime? CheckoutDate { get; set; }
+
+    public DateTime? DueDate { get; set; }
+
+    public int? DaysLeft { get; set; }
+
+    public DateTime? ReturnDate { get; set; }
+
+    public virtual Book? Book { get; set; }
+
+    public virtual Reader? TakenByNavigation { get; set; }
+}
